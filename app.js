fa5fdb38ae3eec768fbb226ae65b95066157d44b
LAKSHMI MANJUNATH <Lakshmi.Manjunath@libertymutual.com>
2020-10-30 04:38:48 -0400

    doodler,platform,start functionality added

--- app.js
+++ app.js
@@ -5,19 +5,68 @@ document.addEventListener('DOMContentLoaded', () => {
     let doodlerLeftSpace = 50;
     let doodlerBottomSpace = 150;
     let isGameOver = false;
+    let platformCount = 5;
+    let platforms = [];
+    let upTimeId;
+    let downTimeId;
 
     function createDoodler(){
         grid.appendChild(doodler);
         doodler.classList.add('doodler');
         doodler.style.left = doodlerLeftSpace + 'px';
         doodler.style.bottom = doodlerBottomSpace + 'px';
-
+       
     }
 
-    function start(){
-        if (!isGameOver){
-            createDoodler();
+    class Platform  {
+        constructor(newPlatBottom){
+            this.bottom = newPlatBottom;
+            //(grid-width - doddler-height) = 400-85 = 315
+            this.left = Math.random() * 315;
+            this.visual = document.createElement('div');
+            
+            const visual= this.visual;
+            visual.classList.add('platform');
+            visual.style.left = this.left + 'px';
+            visual.style.bottom = this.bottom + 'px';
+            grid.appendChild(visual);
         }
     }
-    start();
+        function createPlaforms(){
+            for(let i=0; i< platformCount ; i++){
+                // 600 is the height of the platform
+                let platSpacing = 600/platformCount;
+                let newPlatBottom = 100 + i * platSpacing;
+                let newPlatform = new Platform(newPlatBottom);
+                platforms.push(newPlatform);
+                console.log(platforms);
+            }
+        }
+
+        function movePlatform(){
+            if(doodlerBottomSpace > 200){
+                platforms.forEach(platform => {
+                    platform.bottom -= 4;
+                    let visual = platform.visual
+                    visual.style.bottom = platform.bottom + 'px';
+                });
+
+            }
+        }
+        function jump() {
+            upTimeId = setInterval(function (){
+                doodlerBottomSpace += 20;
+                doodler.style.bottom = doodlerBottomSpace + 'px';
+            }, 30) //invoke it every 30 seconds
+        }
+
+        function start(){
+            if (!isGameOver){
+                createDoodler();
+                createPlaforms();
+                setInterval(movePlatform,30);
+                jump();
+            }
+        }
+        start();
 })
